08f00f9cd2ba93a456484b4ded445656e402f17a
Patroclus <patches72790@gmail.com>
2026-09-23 19:12:12 +0000

    fix cockpit

--- modules/services.nix
+++ modules/services.nix
@@ -16,10 +16,20 @@
     enable = true;
     port = 9090;
     openFirewall = true;
+    plugins = [
+      pkgs.cockpit-podman
+      pkgs.cockpit-machines
+      pkgs.cockpit-files
+    ];
     settings = {
       WebService = {
         AllowUnencrypted = true;
-        Origins = lib.mkForce "http://localhost:9090 https://localhost:9090 http://127.0.0.1:9090";
+
+        # This tells Cockpit how to handle cookie security flags over unencrypted connections
+        ProtocolHeader = "X-Forwarded-Proto"; 
+        
+        # Use http:// explicitly in your Origins list
+        Origins = lib.mkForce "http://127.0.0.1:9090 http://localhost:9090 http://orpheus-nas:9090";
       };
     };
   };
